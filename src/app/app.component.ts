972f7df60fd99d9b17515b88ccf2d295d4e7559f
Frans <frans_421_12@hotmail.com>
2024-02-03 23:14:01 -0500

    feat: Login component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,10 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'gym';
+  cargando: boolean = true;
+
+  // constructor(private ofAuth: AngularfireAuth)
+  // {
+
+  // }
 }
